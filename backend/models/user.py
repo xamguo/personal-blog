936c25823300ae253e9c9e272c60789c41e765ab
xiao-guo <guoxiao113113@gmail.com>
2018-02-28 22:26:37 -0800

    Add migration

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,5 +4,6 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    note = db.Column(db.String(64))
     def __repr__(self):
         return '<User %r>' % self.username
--- manage.py
+++ manage.py
@@ -5,7 +5,9 @@ from flask_migrate import Migrate, MigrateCommand
 from backend.app import create_app, db
 from backend.models.user import User
 
-manager = Manager(create_app())
+app = create_app()
+manager = Manager(app)
+migrate = Migrate(app, db)
 
 def make_shell_context():
   return dict(db=db, User=User)
@@ -14,6 +16,7 @@ def make_shell_context():
 def hello():
     print "hello"
 
+manager.add_command('db', MigrateCommand)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
